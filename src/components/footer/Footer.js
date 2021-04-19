c14742fc700935ba7e75e12477b6bd3ec8445e03
Andrieddu <laiandrea2@gmail.com>
2021-04-19 08:33:59 +0100

    commit on 'main'

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>© 2021 Airbnb Clone | No rights reserved | Demo</p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+      <p>Andrea Lai</p>
+    </div>
+  );
+}
+
+export default Footer;
